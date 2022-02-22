518e6a1cdcbd730f845d15d70c47189b42ae6c74
amani-berrebeh <berrebehamani@gmail.com>
2022-02-22 13:03:21 +0100

    movieapp

---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const movies = [
+    
+    {
+      id:Math.random(),
+      title: "The pianist",
+      description: "Drame,guerre,fiction",
+      posterURL:
+        "https://m.media-amazon.com/images/M/MV5BOWRiZDIxZjktMTA1NC00MDQ2LWEzMjUtMTliZmY3NjQ3ODJiXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+      rating:5,
+
+    },
+    {
+      id:Math.random(),
+      title: "The pianist",
+      description: "Drame,guerre,fiction",
+      posterURL:
+        "https://m.media-amazon.com/images/M/MV5BOWRiZDIxZjktMTA1NC00MDQ2LWEzMjUtMTliZmY3NjQ3ODJiXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+      rating:5,
+
+    },
+    {
+      id:Math.random(),
+      title: "The pianist",
+      description: "Drame,guerre,fiction",
+      posterURL:
+        "https://m.media-amazon.com/images/M/MV5BOWRiZDIxZjktMTA1NC00MDQ2LWEzMjUtMTliZmY3NjQ3ODJiXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+      rating:5,
+
+    },
+    {
+      id:Math.random(),
+      title: "The pianist",
+      description: "Drame,guerre,fiction",
+      posterURL:
+        "https://m.media-amazon.com/images/M/MV5BOWRiZDIxZjktMTA1NC00MDQ2LWEzMjUtMTliZmY3NjQ3ODJiXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+      rating:5,
+
+    }
+
+  ];
+  
+  
